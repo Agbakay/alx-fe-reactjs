9c41ad94308624cbc2bc72f527f95e2fdf339188
Agbakay <kayodeakintunde919@gmail.com>
2025-07-14 15:32:05 +0100

    Added styling

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
-    <footer>
-      <p>© 2023 City Lovers</p>
+    <footer
+      style={{ border: "1px solid gray", padding: "10px ", margin: "10px" }}
+    >
+      <p style={{ fontWeight: "bold" }}>© 2023 City Lovers</p>
     </footer>
   );
 }
