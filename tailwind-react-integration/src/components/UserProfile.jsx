bfc6202cdc72f19ce914da2f8075a2abb63bfafc
Akintunde Kayode <kayodeakintunde919@gmail.com>
2025-08-07 17:20:27 +0100

    Update card styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 mx-auto my-20 rounded-lg max-w-sm shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 mx-auto my-20 rounded-lg max-w-sm shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm hover:shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 ms:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">
+        John Doe
+      </h1>
+      <p className="text-gray-600 text-base sm:text-sm md:text-bae">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
