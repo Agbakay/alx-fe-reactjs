d0813ff5f678567a70ecfa15c62465772e21abf8
Akintunde Kayode <kayodeakintunde919@gmail.com>
2025-09-06 01:01:04 +0100

    New update

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const hnadleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const validate = () => {
+    const newErrors = {};
+    if (!formData.username) newErrors.username = "Username is required";
+    if (!formData.email) newErrors.email = "Email is required";
+    if (!formData.password) newErrors.password = "Password is required";
+    return newErrors;
+  };
+
+  return <div>RegistrationForm</div>;
+};
+
+export default RegistrationForm;
